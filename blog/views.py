aed8cb282c53283fa97c6591a4113d7fe6db8a21
Milena Ruy <milenaruy@hotmail.com>
2018-10-08 10:11:43 -0300

    alteracoes amauri

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 def index(request):
     return render(request, 'index/index.html', {})
+def sobre(request):
+    return render(request, 'sobre/sobre.html', {})
 
 # Create your views here.
