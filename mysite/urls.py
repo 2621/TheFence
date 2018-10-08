ff3a56c0c3c72b2f0eca7eac0d5d066593bb1d1b
Milena Ruy <milenaruy@hotmail.com>
2018-10-08 09:48:24 -0300

    alteracoes amauri

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.urls import path, include
 from django.contrib import admin
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('', include('blog.urls'))
 ]
